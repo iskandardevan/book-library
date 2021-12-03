56dbb609bc1fca8502656c4a6b6e7f4310fff5ec
iskandardevan <devvv13@gmail.com>
2021-12-03 10:29:22 +0700

    fixing staging

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -24,19 +24,25 @@ func (ctrl *RouteControllerList) RouteRegister(e *echo.Echo) {
 	e.POST("user/register", ctrl.UserController.RegisterUser)
 	e.POST("user/login", ctrl.UserController.LoginUser)
 	e.GET("user/:id", ctrl.UserController.GetByID)
+	e.GET("users", ctrl.UserController.GetAllUsers)
 	e.PUT("user/:id", ctrl.UserController.UpdateUserByID)
 	e.DELETE("user/:id", ctrl.UserController.DeleteUserByID)
 
 
 	e.POST("author/add", ctrl.AuthorController.AddAuthor)
+	e.GET("authors", ctrl.AuthorController.GetAllAuthors)
+	e.DELETE("author/:id", ctrl.AuthorController.Delete)
 
 	e.POST("publisher/add", ctrl.PublisherController.AddPublisher)
+	e.GET("publishers", ctrl.PublisherController.AddPublisher)
+	e.DELETE("publisher/:id", ctrl.PublisherController.Delete)
 
 	e.POST("book/add", ctrl.BookController.AddBook)
 	e.GET("books", ctrl.BookController.GetAllBooks)
+	e.DELETE("book/:id", ctrl.BookController.Delete)
 
 	e.POST("reservation/add", ctrl.ReservationController.AddReservation)
 	e.GET("reservations", ctrl.ReservationController.GetAllReservations)
-	
+	e.DELETE("reservation/:id", ctrl.ReservationController.Delete)
 	
 }
